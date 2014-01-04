2fd7e463bf11d3a065336cf99208677ea0b03541
Chris Hudson <chris@cjhudson.com>
2014-01-03 22:34:04 -0500

    Merged TextMessage.html and SendMessage.php, moved sid and token info to private file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 TextSender
 ==========
 
-TextSender is a simple web app SMS text message sender to help understand and use the Twilio API.
+TextSender is a simple web app SMS text message sender to help understand and use the Twilio API, written in PHP and integrated into a wordpress page as a shortcode.
--- SendMessage.php
+++ SendMessage.php
@@ -1,4 +1,12 @@
+<html>
+<!-- add style to form in wordpress -->
+<body>
 
+<form action="SendMessage.php" method="post">
+To Number: <input type="text" name="ToNumber"><br>
+Message: <input type="text" name="UserMessage"><br>
+<input type="submit">
+</form>
 
 <?php
 // Get variables from form input
@@ -9,14 +17,22 @@ $MediaURL = ""; //url for image like heart in example ?
 
 // Get the PHP helper library from twilio.com/docs/php/install
 require_once('/path/to/twilio-php/Services/Twilio.php'); // Loads the library
+
+//-------------------------------------- How to make a call from AccountIDToke.php to grab code
 // Your Account Sid and Auth Token from twilio.com/user/account - check gitingore code
+require_once('/AccountIDToke.php'); // loads hidden Sid and Auth token variables
+$sid = $_GET['sid'];
+$token = $_GET['token'];
+//-----------------------------------------------------------------
 
 $client = new Services_Twilio($sid, $token);
 
 $client->account->messages->sendMessage("$FromNumber", "$ToNumber", "$UserMessage", "$MediaURL");
 
 
-echo "Your message ". $UserMessage . "to phone number ". $ToNumber . "has been sent!";
-
+echo "Your message -". $UserMessage . "- to phone number ". $ToNumber . "has been sent!";
 
 ?>
+
+</body>
+</html> 
